9f034551e657ea8333dfee1cc3e34dbe9b476b55
mahmoudatef <mahmoud97atef@gmail.com>
2025-10-28 01:42:55 +0300

    feat(trips): add trips cubit and state management
    Implement cubit for trips feature including state management and error handling
    Add assets directory configuration in pubspec.yaml

---
+++ lib/features/trips/presentation/cubit/trips_cubit.dart
@@ -0,0 +1,22 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:users/features/trips/domain/entities/trip.dart';
+import 'package:users/features/trips/domain/usecases/get_trips.dart';
+
+import 'trips_state.dart';
+
+class TripsCubit extends Cubit<TripsState> {
+  final GetTrips getTrips;
+
+  TripsCubit({required this.getTrips}) : super(TripsInitial());
+
+  Future<void> fetchTrips() async {
+    try {
+      emit(TripsLoading());
+      final trips = await getTrips();
+      emit(TripsLoaded(trips));
+    } catch (e) {
+      emit(TripsError(e.toString()));
+    }
+  }
+}
---
+++ lib/features/trips/presentation/cubit/trips_state.dart
@@ -0,0 +1,32 @@
+import 'package:equatable/equatable.dart';
+
+import '../../domain/entities/trip.dart';
+
+abstract class TripsState extends Equatable {
+  const TripsState();
+
+  @override
+  List<Object> get props => [];
+}
+
+class TripsInitial extends TripsState {}
+
+class TripsLoading extends TripsState {}
+
+class TripsLoaded extends TripsState {
+  final List<Trip> trips;
+
+  const TripsLoaded(this.trips);
+
+  @override
+  List<Object> get props => [trips];
+}
+
+class TripsError extends TripsState {
+  final String message;
+
+  const TripsError(this.message);
+
+  @override
+  List<Object> get props => [message];
+}
